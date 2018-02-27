c9fb6379fb51e4bd6a9b2a690207428fddefaa79
Stefan Ziegler <edi.gonzales@gmail.com>
2018-02-27 17:36:25 +0100

    safety

---
+++ oereb-app-schema-ddl-additional.sql
@@ -0,0 +1,6 @@
+
+GRANT USAGE ON SCHEMA av_avdpool_ng TO oereb_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA av_avdpool_ng TO oereb_read;
+
+GRANT USAGE ON SCHEMA av_plzortschaft TO oereb_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA av_plzortschaft TO oereb_read;
